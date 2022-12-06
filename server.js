e8dc60936d5a9a3ff27fafc0dd59d059968a1c98
Oleh Medvid <meandjob@protonmail.com>
2022-12-06 23:07:32 +0200

    add: connect mongoDB to project

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const app = require("./app");
-
+const mongoose = require("mongoose");
+// Password gzzO7DSY4QtKXGec
 const { PORT = 3000 } = process.env;
+const DB_HOST =
+  "mongodb+srv://admin:gzzO7DSY4QtKXGec@cluster0.vnec5ua.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("database connect success"))
+  .catch((error) => console.log(error.message));
 
 app.listen(PORT, () => {
   console.log("Server running. Use our API on port: 3000");
