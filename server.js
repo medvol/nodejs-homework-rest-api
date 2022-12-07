06906afbb6748224ddc88d1497c4a8ccb994c5b7
Oleh Medvid <meandjob@protonmail.com>
2022-12-07 23:44:57 +0200

    add: .env and dotenv

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 // Password gzzO7DSY4QtKXGec
-const { PORT = 3000 } = process.env;
-const DB_HOST =
-  "mongodb+srv://admin:gzzO7DSY4QtKXGec@cluster0.vnec5ua.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => console.log("database connect success"))
-  .catch((error) => console.log(error.message));
-
-app.listen(PORT, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
