c3a6153d175ad2f262312d429ef56c914749fb53
Oleh Medvid <meandjob@protonmail.com>
2022-12-21 23:09:05 +0200

    add: default avatar to user and public folder

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,22 +1,24 @@
 const { Conflict } = require("http-errors");
+const gravatar = require("gravatar");
 
 const { User } = require("../../models/user");
 
 const register = async (req, res) => {
-  const { email, password} = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
     throw new Conflict("Email in use");
-    }
-    
-  const newUser = new User({ email});
+  }
+  const avatarURL = gravatar.url(email);
+  const newUser = new User({ email, avatarURL });
   await newUser.setPassword(password);
   await newUser.save();
 
   res.status(201).json({
     user: {
       email,
-      subscription:newUser.subscription
+      subscription: newUser.subscription,
+      avatarUrl: newUser.avatarURL,
     },
   });
 };
